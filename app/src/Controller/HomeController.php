da0a5109e5b1fbdb176eeab9e671935289e38f5d
Simone D'Amico <sd@ideato.it>
2020-03-22 17:49:54 +0100

    first route: home page

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
