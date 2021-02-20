3641533dd4507605375c3948df6cfa155e53220a
Simone D'Amico <simone.damico@spreaker.com>
2021-02-20 17:46:00 +0100

    Fix style guide

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -7,29 +7,29 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-    class AppFixtures extends Fixture
+class AppFixtures extends Fixture
+{
+    /** @var UserPasswordEncoderInterface */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
-        /** @var UserPasswordEncoderInterface */
-        private $passwordEncoder;
-
-        public function __construct(UserPasswordEncoderInterface $passwordEncoder)
-        {
-            $this->passwordEncoder = $passwordEncoder;
-        }
-
-        public function load(ObjectManager $manager)
-        {
-            $ryan = new User();
-            $ryan->setEmail('ryan@ryan.it');
-            $ryan->setPassword($this->passwordEncoder->encodePassword($ryan, 'helloworld'));
-
-            $julie = new User();
-            $julie->setEmail('julie@julie.it');
-            $julie->setPassword($this->passwordEncoder->encodePassword($julie, 'helloworld'));
-
-            $manager->persist($ryan);
-            $manager->persist($julie);
-
-            $manager->flush();
-        }
+        $this->passwordEncoder = $passwordEncoder;
     }
+
+    public function load(ObjectManager $manager)
+    {
+        $ryan = new User();
+        $ryan->setEmail('ryan@ryan.it');
+        $ryan->setPassword($this->passwordEncoder->encodePassword($ryan, 'helloworld'));
+
+        $julie = new User();
+        $julie->setEmail('julie@julie.it');
+        $julie->setPassword($this->passwordEncoder->encodePassword($julie, 'helloworld'));
+
+        $manager->persist($ryan);
+        $manager->persist($julie);
+
+        $manager->flush();
+    }
+}
